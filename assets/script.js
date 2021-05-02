d2a3b2f9d7b4b46dff180169cc0c766cdc708591
Tim <SESA493999@se.com>
2021-05-02 16:03:51 +1000

    All links added and working

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,14 @@ let currentTime = moment().format('MMMM Do, h:mm a');
 $('#time').html(`My local time is: ${currentTime}`)
 
 //Links
-$('#projAsite').attr('href',)
-$('#projBsite').attr('href',)
-$('#projCsite').attr('href',)
-$('#projAgit').attr('href',)
-$('#projBgit').attr('href',)
-$('#projCgit').attr('href',)
-$('#github').attr('href',)
-$('#linkdin').attr('href',)
-$('#resume').attr('href',)
+$('#projAsite').attr('href', 'https://tbr2000.github.io/The-ANZAC-travel-Bubble_App/')
+$('#projBsite').attr('href','https://github.com/TBR2000/day-planner')
+$('#projCsite').attr('href', 'https://github.com/TBR2000/password-generator')
+$('#projAgit').attr('href','https://github.com/TBR2000/The-ANZAC-travel-Bubble_App')
+$('#projBgit').attr('href', 'https://tbr2000.github.io/day-planner/')
+$('#projCgit').attr('href', 'https://tbr2000.github.io/password-generator/')
+$('#github').attr('href', 'https://github.com/TBR2000')
+$('#linkdin').attr('href', 'https://www.linkedin.com/in/timothy-rabbidge-a28202211/')
+$('#resume').attr('href', 'https://drive.google.com/file/d/1fyec2OKBESTBLSqlpj7vSCl4ewg5sSPv/view?usp=sharing')
 
 
