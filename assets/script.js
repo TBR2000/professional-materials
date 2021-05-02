599435bb2de5bcdacfc066274353da2c4ddcbc58
Tim <SESA493999@se.com>
2021-05-02 11:18:24 +1000

    time added, custom html started

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// display local time
+let currentTime = moment().format('MMMM Do, h:mm a');
+$('#time').html(`My local time is: ${currentTime}`)
+
+//Links
+$('#projAsite').attr('href',)
+$('#projBsite').attr('href',)
+$('#projCsite').attr('href',)
+$('#projAgit').attr('href',)
+$('#projBgit').attr('href',)
+$('#projCgit').attr('href',)
+$('#github').attr('href',)
+$('#linkdin').attr('href',)
+
+
